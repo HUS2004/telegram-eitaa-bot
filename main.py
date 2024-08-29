60442f036b0c25f588aad1f1fef5498f3cd43af6
Hussein Husseini <lantern120am@gmail.com>
2024-08-29 20:21:04 +0330

    therd

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import subprocess
+import concurrent.futures
 
 # مسیرهای فایل‌های پایتون که می‌خواهید اجرا کنید
 scripts = [
     "get-telegram.py",
     "translate.py",
-    "send-eita.py"
+    "send-eita.py",
     "clear.py"
 ]
 
-# اجرای فایل‌ها به صورت همزمان و باز کردن هر کدام در یک پنجره‌ی ترمینال جداگانه
-processes = []
-for script in scripts:
-    process = subprocess.Popen(["start", "cmd", "/k", "python", script], shell=True)
-    processes.append(process)
+def run_script(script):
+    subprocess.run(["python", script], check=True)
+
+def main():
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(run_script, script) for script in scripts]
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                future.result()  # بررسی نتیجه و استثناء
+            except Exception as e:
+                print(f"Script generated an exception: {e}")
+
+if __name__ == "__main__":
+    main()
